28b64f70d70a191de116cc032b8b5d289267b384
Jack Hoffman <32367528+Jacker71300@users.noreply.github.com>
2019-12-04 14:07:22 -0500

    added a blank canvas to the scene

--- project3/js/main.js
+++ project3/js/main.js
@@ -1,12 +1,36 @@
 "use strict";
-const app = new PIXI.Application(600,600);
-document.body.appendChild(app.view);
+const app = new PIXI.Application(600,740);
+let container = document.querySelector("#gamespace");
+container.appendChild(app.view);
 
 const sceneWidth = app.view.width;
 const sceneHeight = app.view.height;
+
+PIXI.loader.load(setup);
 
 let stage;
 
 let menuScene;
 let gameScene;
 let gameOverScene;
+
+function setup(){
+    stage = app.stage;
+
+    menuScene = new PIXI.Container();
+    stage.addChild(menuScene);
+
+    gameScene = new PIXI.Container();
+    gameScene.visible = false;
+    stage.addChild(gameScene);
+
+    gameOverScene = new PIXI.Container();
+    gameOverScene.visible = false;
+    stage.addChild(gameOverScene);
+
+    addTextAndButtons();
+}
+
+function addTextAndButtons(){
+    
+}
