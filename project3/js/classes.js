c0abddd56557db48cd3c6fba477679e3671d06e8
Justin Pollak <jmp1203@ad.rit.edu>
2019-12-04 15:00:48 -0500

    For loop

--- project3/js/classes.js
+++ project3/js/classes.js
@@ -19,7 +19,7 @@ class ColorPeg extends PIXI.Graphics{
 }
 
 class Row extends PIXI.Graphics{
-    constructor(x = 100, y = 0, color = 0x00FF00){
+    constructor(x = 100, y = 55, color = 0x999999){
         super();
         this.beginFill(color);
         this.drawRect(x, y, 400, 50);
@@ -34,51 +34,55 @@ class Row extends PIXI.Graphics{
         let hole4 = new PIXI.Graphics();
 
         hole1.beginFill(colorArray[0]);
-        hole1.drawCircle(this.x + 10, this.y + 5, 20);
+        hole1.drawCircle(x + 30, y + 25, 20);
         hole1.endFill();
         
         hole2.beginFill(colorArray[1]);
-        hole2.drawCircle(this.x + 70, this.y + 5, 20);
+        hole2.drawCircle(x + 90, y + 25, 20);
         hole2.endFill();
         
         hole3.beginFill(colorArray[2]);
-        hole3.drawCircle(this.x + 130, this.y + 5, 20);
+        hole3.drawCircle(x + 150, y + 25, 20);
         hole3.endFill();
         
         hole4.beginFill(colorArray[3]);
-        hole4.drawCircle(this.x + 190, this.y + 5, 20);
+        hole4.drawCircle(x + 210, y + 25, 20);
         hole4.endFill();
 
         hole1.on("pointerup", colorArray[0] = 0x555555);
         hole2.on("pointerup", colorArray[1] = 0x555555);
         hole3.on("pointerup", colorArray[2] = 0x555555);
         hole4.on("pointerup", colorArray[3] = 0x555555);
+    
+        let answer1 = new PIXI.Graphics;
+        let answer2 = new PIXI.Graphics;
+        let answer3 = new PIXI.Graphics;
+        let answer4 = new PIXI.Graphics;
+        
+        answer1.beginFill(answerArray[0]);
+        answer1.drawCircle(x + 250, y + 15, 8)
+        answer1.endFill();
+    
+        answer2.beginFill(answerArray[1]);
+        answer2.drawCircle(x + 270, y + 15, 8)
+        answer2.endFill();
+    
+        answer3.beginFill(answerArray[2]);
+        answer3.drawCircle(x + 250, y + 35, 8)
+        answer3.endFill();
+    
+        answer4.beginFill(answerArray[3]);
+        answer4.drawCircle(x + 270, y + 35, 8)
+        answer4.endFill();
 
         this.addChild(hole1);
         this.addChild(hole2);
         this.addChild(hole3);
         this.addChild(hole4);
-    
-        let answer1;
-        let answer2;
-        let answer3;
-        let answer4;
-        
-        answer1.beginFill(answerArray[0]);
-        answer1.drawCircle(this.x + 240, this.y + 5, 8)
-        answer1.endFill();
-    
-        answer2.beginFill(answerArray[1]);
-        answer2.drawCircle(this.x + 250, this.y + 5, 8)
-        answer2.endFill();
-    
-        answer3.beginFill(answerArray[2]);
-        answer3.drawCircle(this.x + 240, this.y + 25, 8)
-        answer3.endFill();
-    
-        answer4.beginFill(answerArray[3]);
-        answer4.drawCircle(this.x + 250, this.y + 25, 8)
-        answer4.endFill();
+        this.addChild(answer1);
+        this.addChild(answer2);
+        this.addChild(answer3);
+        this.addChild(answer4);
     }
 
     updateColors(){
@@ -90,5 +94,19 @@ class Row extends PIXI.Graphics{
 
     compareRow(){
 
+    }
+}
+
+class Key extends PIXI.Graphics{
+    constructor(x = 150, y = 0, color = 0x999999){
+        super();
+        this.beginFill(color);
+        this.drawRect(x, y, 300, 50);
+        this.endFill();
+        let colorArray = [];
+
+        for(let i = 0; i < 4; i++){
+            let colorNum = Math.random() * 6;
+        }
     }
 }
